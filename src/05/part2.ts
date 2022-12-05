eb490d544c0c5670142eb91e6018f983fc4177a1
Nick Warters <nick.warters@gmail.com>
2022-12-05 18:52:38 +0000

    Day 5 Part 2

--- src/05/part2.ts
+++ src/05/part2.ts
@@ -1,14 +1,82 @@
 import { readFileSync } from 'fs'
 
-const tests: [string, any][] = [[``, 0]]
+function solve(input: string): any {
+	const lines = input.split('\n')
+
+	const firstInstructionLine = lines.findIndex((x) => x.startsWith('move'))
+	const stackCount = parseInt(
+		lines[firstInstructionLine - 2]
+			.match(/\d/g)!
+			.sort((a, b) => parseInt(b) - parseInt(a))[0]
+	)
+
+	const stacks = new Array(stackCount)
+
+	let stackIndex = 0
+	for (let r = 0; r < firstInstructionLine - 2; r++) {
+		stackIndex = 0
+		let stack = new Array()
+		let item: string[] | null
+		for (let c = 2; c < lines[r].length; c += 4) {
+			item = lines[r].substring(c - 2, c).match(/([A-Z])/g)
+			if (item) {
+				if (!stacks[stackIndex]) {
+					stacks[stackIndex] = new Array()
+				}
+				stacks[stackIndex].unshift(item)
+			}
+			stackIndex++
+		}
+	}
+
+	const instructions = lines.slice(firstInstructionLine)
+
+	for (let i = 0; i < instructions.length; i++) {
+		if (!instructions[i]) break
+		const [c, f, t] = instructions[i].match(/\d{1,}/g)!
+
+		let count = parseInt(c)
+		const from = parseInt(f)
+		const to = parseInt(t)
+
+		stacks[to - 1].push(
+			...stacks[from - 1].splice(stacks[from - 1].length - count, count)
+		)
+	}
+
+	const res = stacks.reduce((prev, next) => {
+		return (prev += next.pop())
+	}, '')
+
+	return res
+}
+
+const tests: [string, any][] = [
+	[
+		`    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`,
+		'MCD',
+	],
+]
 
 tests.forEach(([testData, expected]) => {
-    const result = solve(testData)
-    console.log(`Example Input Solution - Expected: ${expected}, Got: ${result}, ${result === expected ? 'PASS' : 'FAIL'}`)
+	const result = solve(testData)
+	console.log(
+		`Example Input Solution - Expected: ${expected}, Got: ${result}, ${
+			result === expected ? 'PASS' : 'FAIL'
+		}`
+	)
 })
 
-console.log('Full Input Solution', solve(readFileSync('./input.txt', { encoding: 'utf-8' })))
+console.log(
+	'Full Input Solution',
+	solve(readFileSync('./input.txt', { encoding: 'utf-8' }))
+)
 
-function solve(input: string): any {
-    throw new Error('function not implemented')
-}
