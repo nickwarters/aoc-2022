56c67d2db7ef223e6c165d60724ac64456a60ebe
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2022-12-10 09:28:54 +0000

    Day 10 Part 2

---
+++ src/10/part2.ts
@@ -0,0 +1,191 @@
+import { readFileSync } from 'fs'
+
+const tests: [string, any][] = [[`addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`, `\n##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`]]
+
+tests.forEach(([testData, expected]) => {
+    const result = solve(testData)
+    console.log(`Example Input Solution - Expected: ${expected}, Got: ${result}, ${result === expected ? 'PASS' : 'FAIL'}`)
+})
+
+console.log('Full Input Solution', solve(readFileSync('./input.txt', { encoding: 'utf-8' })))
+
+function solve(input: string): any {
+
+    let currentCycle = 0
+    let spriteS = 0 
+    let spriteE = 2 
+    let cnt = 1
+    const isVisible = () => spriteS <= currentCycle - 1 && spriteE >= currentCycle - 1
+
+    let output = ''
+
+    input.split('\n').forEach(line => {
+        if(line === '') return
+        
+        cnt = line !== 'noop' ? 2 : 1 
+
+        for(let i = 0; i < cnt; i++){
+            currentCycle +=  currentCycle === 40 ? -39: 1
+            output = `${output}${currentCycle === 1 ? '\n': ''}${isVisible() ? '#' : '.'}`
+        }
+        
+        if(line === 'noop') return 
+
+        const val = parseInt(line.split(' ')[1]) 
+        spriteS = spriteS += val 
+        spriteE = spriteE += val 
+
+    })
+
+    return output
+}
