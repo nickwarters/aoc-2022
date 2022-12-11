4cf8fe086ad2fbc21f009a8bb866f8206f5c287f
Nick Warters <nick.warters@gmail.com>
2022-12-11 19:44:15 +0000

    Day 9 Part 2

--- src/09/part2.ts
+++ src/09/part2.ts
@@ -1,4 +1,3 @@
-import { match } from 'assert'
 import { readFileSync } from 'fs'
 
 const tests: [string, any][] = [
@@ -28,98 +27,77 @@ U 20`,
 
 tests.forEach(([testData, expected]) => {
 	const result = solve(testData)
-	console.log(
+	process.stdout.write(
 		`Example Input Solution - Expected: ${expected}, Got: ${result}, ${
 			result === expected ? 'PASS' : 'FAIL'
-		}`
+		}\n`
 	)
 })
 
-//console.log('Full Input Solution', solve(readFileSync('./input.txt', { encoding: 'utf-8' })))
+process.stdout.write(`Full Input Solution: ${solve(readFileSync('./input.txt', { encoding: 'utf-8' }))}\n`)
+
+type S = [number, number]
 
 function solve(input: string): any {
 	const lines = input.split('\n')
 	const visited = new Set()
 
-	// const getDestination = ([r,c], d, cnt) = {
-
-	// }
-	const current = new Array(10)
-	for (let ti = 0; ti < current.length; ti++) {
-		current[ti] = [0, 0]
+	const current: S[] = new Array(10)
+	for (let i = 0; i < current.length; i++) {
+		current[i] = [0, 0]
 	}
+
+    process.stdout.write(`${JSON.stringify(current)}\n`)
+
+    const transforms = {U: [1,0], D: [-1,0], L: [0,-1], R: [0, 1]}
+
+    const shouldMove = (prev: S, next: S): boolean => {
+        return Math.abs(prev[0] - next[0]) > 1 || Math.abs(prev[1] - next[1]) > 1
+    }
+
+    const moveHead = (head: S, d: 'U'|'D'|'L'|'R'): S => {
+        const t = transforms[d]!
+
+        return [head[0] + t[0], head[1] + t[1]]
+    }
+
+    const moveKnot = (prev: S, next: S): S => {
+        if(!shouldMove(prev, next)) return next 
+
+        if(Math.abs(prev[0] - next[0]) === 2 && Math.abs(prev[1] - next[1]) === 2){
+            return [Math.floor((prev[0] + next[0]) / 2),Math.floor((prev[1] + next[1]) / 2)  ]
+        }
+
+        if(Math.abs(prev[0] - next[0]) === 2){
+                return [Math.floor((prev[0] + next[0]) / 2), prev[1]]
+        } 
+        if(Math.abs(prev[1] - next[1]) === 2){
+            return [prev[0], Math.floor((prev[1] + next[1]) / 2)]
+        } 
+
+        return next
+    }
 
 	for (let i = 0; i < lines.length; i++) {
-		const [d, count] = lines[i].split(' ').map((x, ind) => {
-			if (ind === 0) return x
-			return parseInt(x)
-		}) as [string, number]
+        if(lines[i] === '') continue
 
-		console.log({ d, count })
+		const d = lines[i].charAt(0) as keyof typeof transforms
+        let count = parseInt(lines[i].split(' ')[1])
 
-		for (let c = 0; c < count; c++) {
-			if (d === 'U') {
-				// console.log(`moving head to [${current[0][0] + 1},${current[0][1]}]`)
-				current[0][0] += 1
-			}
-			if (d === 'L') {
-				// console.log(`moving head to [${current[0][0]},${current[0][1] - 1}]`)
-				current[0][1] -= 1
-			}
-			if (d === 'R') {
-				// console.log(`moving head to [${current[0][0]},${current[0][1] + 1}]`)
-				current[0][1] += 1
-			}
-			if (d === 'D') {
-				// console.log(`moving head to [${current[0][0] - 1},${current[0][1]}]`)
-				current[0][0] -= 1
-			}
-			console.group('--> start iteration over knots <--')
-			for (let t = 1; t < current.length; t++) {
-				let movedV = false
-				let movedH = false
+        while (count){
+           current[0] = moveHead(current[0], d) 
 
-				if (
-					Math.abs(current[t - 1][0] - current[t][0]) > 1 &&
-					Math.abs(current[t - 1][1] - current[t][1]) > 1
-				) {
-					current[t] = [
-						Math.floor((current[t - 1][0] + current[t][0]) / 2),
-						Math.floor((current[t - 1][1] + current[t][1]) / 2),
-					]
-				}
+           for(let c = 1; c < current.length; c++){
+               current[c] = moveKnot(current[c-1], current[c])
 
-				if (Math.abs(current[t - 1][0] - current[t][0]) > 1) {
-					current[t] = [
-						Math.floor((current[t - 1][0] + current[t][0]) / 2),
-						current[t][1],
-					]
-				}
+               if(c === 9){
 
-				if (Math.abs(current[t - 1][1] - current[t][1]) > 1) {
-					current[t] = [
-						current[t][0],
-						Math.floor((current[t - 1][1] + current[t][1]) / 2),
-					]
-				}
-
-				visited.add(`${current[9][0]},${current[9][1]}`)
-				if (t === 9) {
-					console.log({ pos: current[9], visted: visited.size })
-				}
-				// console.log({
-				// 	d,
-				// 	count,
-				// 	currentH: current[t - 1],
-				// 	current: current[t],
-				// 	visitedCount: visited.size,
-				// })
-			}
-			console.log('--> end iteration over knots <--')
-			console.groupEnd()
-			// console.log(current)
-		}
+               visited.add(`[${current[c][0]},${current[c][1]}]`)}
+           }
+           count--
+        }
 	}
-
 	return visited.size
 }
+
+
